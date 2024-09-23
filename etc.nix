cbf1acd1d5bf86d49c63b321d78ca8714c9d60a1
Blake Patteson <bpatteson@me.com>
2024-09-22 21:34:47 -0400

    update kitty keybinds

--- etc.nix
+++ etc.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   environment.etc."xdg/kitty/kitty.conf".text = ''
     font_family Victor Mono
@@ -8,7 +7,6 @@
     scrollback_lines 10000
     allow_remote_control yes
     map ctrl+shift+m show_scrollback
-
     map ctrl+v paste_from_clipboard
     map ctrl+c copy_to_clipboard
     map ctrl+n new_os_window
@@ -20,5 +18,17 @@
     map ctrl+equal change_font_size all +2.0
     map ctrl+minus change_font_size all -2.0
     map ctrl+backspace change_font_size all 0
+
+    # New scrolling mappings
+    map ctrl+u scroll_page_up
+    map ctrl+d scroll_page_down
+
+    # Vi mode specific mappings
+    map ctrl+shift+j scroll_line_down
+    map ctrl+shift+k scroll_line_up
+    map ctrl+shift+h scroll_to_prompt -1
+    map ctrl+shift+l scroll_to_prompt 1
+    map ctrl+shift+g scroll_to_prompt 0
+    map ctrl+shift+G scroll_to_prompt -1
   '';
 }
