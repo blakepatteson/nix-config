a804d9447207d0c4f06af2d3d3f972107a6feb9a
Blake Patteson <bpatteson@me.com>
2024-12-03 07:58:52 -0500

    rm nvidia prime settings, etc

--- boot.nix
+++ boot.nix
@@ -12,6 +12,7 @@ in
         sudo nixos-rebuild switch
     '';
   }];
+  boot.plymouth.enable = false;
   boot.loader = {
     systemd-boot = { enable = true; configurationLimit = 10; };
     efi.canTouchEfiVariables = true;
@@ -85,8 +86,9 @@ in
   services.displayManager.defaultSession = "cinnamon";
 
   hardware.opengl.enable = true;
+  hardware.nvidia.open = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  hardware.nvidia.modesetting.enable = true;
+  # hardware.nvidia.modesetting.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   services.locate = {
     enable = true;
@@ -94,17 +96,14 @@ in
     interval = "hourly"; # how often to update the database
     localuser = null; # run updatedb as root
   };
-  hardware.nvidia.prime = {
-    offload.enable = true;
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
-  };
+  # hardware.nvidia.prime = {
+  #   offload.enable = true;
+  #   intelBusId = "PCI:0:2:0";
+  #   nvidiaBusId = "PCI:1:0:0";
+  # };
   services.printing.enable = true;
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
   fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-cjk
     noto-fonts-emoji
     liberation_ttf
     fira-code
