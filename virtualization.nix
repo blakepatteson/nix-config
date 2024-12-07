ddae7e3b6dcaab7183e5c8309bfb28b5d9ea210a
Blake Patteson <bpatteson@me.com>
2024-12-07 13:19:47 -0500

    update virtualization to support tpm boot

--- virtualization.nix
+++ virtualization.nix
@@ -4,9 +4,16 @@
     libvirtd = {
       enable = true;
       qemu.package = pkgs.qemu_kvm;
+      qemu.swtpm.enable = true;
+      qemu.ovmf = {
+        enable = true;
+        packages = [ (pkgs.OVMF.override { secureBoot = true; tpmSupport = true; }).fd ];
+      };
     };
+
     docker.enable = true;
     spiceUSBRedirection.enable = true;
   };
   services.spice-vdagentd.enable = true;
+  security.tpm2 = { enable = true; pkcs11.enable = true; };
 }
